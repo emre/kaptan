925a61168227c678b363290c0403973983247ab5
emre <mail@emreyilmaz.me>
2013-05-14 19:23:32 +0300

    bump version

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='kaptan',
+    version='0.3',
+    packages=['kaptan', 'kaptan.handlers'],
+    url='http://github.com/emre/kaptan',
+    license='MIT',
+    author='Emre Yilmaz',
+    author_email='mail@emreyilmaz.me',
+    description='Configuration Manager',
+    requires=['PyYAML', ],
+)
