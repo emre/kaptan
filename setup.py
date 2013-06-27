b8329e210e96aa42c7503a0172328157b7b028fe
emre <mail@emreyilmaz.me>
2013-06-27 09:36:54 +0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='kaptan',
-    version='0.5',
+    version='0.5.1',
     packages=['kaptan', 'kaptan.handlers'],
     url='http://github.com/emre/kaptan',
     license='MIT',
