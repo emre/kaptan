0fcc690ddcf501dcb4935ed4afadf10c85fc0c36
emre <mail@emreyilmaz.me>
2013-10-07 16:07:39 +0300

    version bump: python3 support added. thanks @berkerpeksag

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='kaptan',
-    version='0.5.4',
+    version='0.5.5',
     packages=find_packages(),
     url='https://github.com/emre/kaptan',
     license='BSD',
