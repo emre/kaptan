007a2abe01eb9fd1405bc83c2dcb4b7d2ed2afea
Tony Narlock <tony@git-pull.com>
2016-05-25 17:58:55 -0500

    Update setup.py to read requirements files

--- setup.py
+++ setup.py
@@ -6,12 +6,27 @@
     :license: BSD, see LICENSE for more details.
 """
 
+import sys
+
 from setuptools import find_packages, setup
 
 about = {}
 with open("kaptan/__about__.py") as fp:
     exec(fp.read(), about)
 
+with open('requirements/base.txt') as f:
+    install_reqs = [line for line in f.read().split('\n') if line]
+
+with open('requirements/test.txt') as f:
+    tests_reqs = [line for line in f.read().split('\n') if line]
+
+if sys.version_info < (2, 7):
+    install_reqs += ['argparse']
+
+if sys.version_info[0] > 2:
+    readme = open('README.rst', encoding='utf-8').read()
+else:
+    readme = open('README.rst').read()
 
 setup(
     name=about['__title__'],
@@ -22,7 +37,9 @@ setup(
     author=about['__author__'],
     author_email=about['__email__'],
     description=about['__description__'],
-    install_requires=['PyYAML'],
+    long_description=readme,
+    install_requires=install_reqs,
+    tests_require=tests_reqs,
     entry_points=dict(
         console_scripts=[
             'kaptan = kaptan:main',
@@ -30,6 +47,9 @@ setup(
     ),
     classifiers=(
         'Development Status :: 5 - Production/Stable',
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
