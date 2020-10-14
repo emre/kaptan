c77cb647bbc4fb5b9302f286f99c6156aef646f5
Po-Chuan Hsieh <sunpoet@sunpoet.net>
2020-10-14 21:23:31 +0000

    Remove warning message
    The warning is as follows:
    Warning: 'classifiers' should be a list, got type 'tuple'
    
    Reference:	https://www.python.org/dev/peps/pep-0301/#distutils-trove-classification

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
             'kaptan = kaptan:main',
         ],
     ),
-    classifiers=(
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Operating System :: POSIX",
         "Operating System :: MacOS :: MacOS X",
@@ -71,5 +71,5 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-    ),
+    ],
 )
